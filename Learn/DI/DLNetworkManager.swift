22d0bcc7bf92209a3bb2a403e4786a3e8fe28853
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2023-12-04 08:55:29 +0700

    add DI

---
+++ Learn/DI/DLNetworkManager.swift
@@ -0,0 +1,50 @@
+//
+//  Network.swift
+//  L-Swift
+//
+//  Created by darktech4 on 28/11/2023.
+//
+
+import SwiftUI
+import Combine
+
+class NetworkingManager: ObservableObject, NetworkingManagerProtocol{
+    
+    enum NetworkingError: LocalizedError{
+        case badURLResponse(url: URL)
+        case unowned
+        
+        var errorDescription: String?{
+            switch self{
+            case .badURLResponse(url: let url): return "Bad response from url: \(url)"
+            case .unowned: return "unowned error occured"
+            }
+        }
+    }
+    
+     func download(url: URL) -> AnyPublisher<Data,Error>{
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try self.handleURLResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+     func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data{
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else{
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+     func handleCompeltion(completion: Subscribers.Completion<Error>){
+        switch completion{
+        case .finished:
+            break
+        case .failure(let err):
+            print(err.localizedDescription)
+        }
+    }
+    
+}
---
+++ Learn/DI/DLView.swift
@@ -0,0 +1,18 @@
+//
+//  DLView.swift
+//  IOS17-Swift
+//
+//  Created by darktech4 on 01/12/2023.
+//
+
+import SwiftUI
+
+struct DLView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DLView()
+}
