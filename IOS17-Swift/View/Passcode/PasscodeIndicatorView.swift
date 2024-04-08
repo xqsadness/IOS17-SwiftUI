288a7146ec6883c32ebb254a58c2473f65e794a2
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-04-08 21:50:21 +0700

    animation passcode

--- IOS17-Swift/View/Passcode/PasscodeIndicatorView.swift
+++ IOS17-Swift/View/Passcode/PasscodeIndicatorView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 
 struct PasscodeIndicatorView: View {
     @Binding var passcode: String
+    @Binding var isAnimation: Bool
+    
     var body: some View {
         HStack(spacing: 32){
             ForEach(0 ..< 4){ index in
@@ -15,9 +17,7 @@ struct PasscodeIndicatorView: View {
                     }
             }
         }
+        .offset(x: isAnimation ? -10 : 0)
+        .animation(Animation.easeInOut(duration: 0.1).repeatCount(2), value: isAnimation)
     }
-}
-
-#Preview {
-    PasscodeIndicatorView(passcode: .constant(""))
 }
--- IOS17-Swift/View/Passcode/PasscodeView.swift
+++ IOS17-Swift/View/Passcode/PasscodeView.swift
@@ -25,9 +25,21 @@ struct PasscodeHomeView: View {
 struct PasscodeView: View {
     @State private var passcode = ""
     @Binding var isAuthenticated: Bool
+    
+    @State private var isWrong = false
+    @State private var isAnimation: Bool = false {
+        didSet {
+            if(isAnimation == true){
+                DispatchQueue.main.asyncAfter(deadline: .now () + 0.25){
+                    isAnimation = false
+                }
+            }
+        }
+    }
+    
     var body: some View {
-        VStack(spacing: 48){
-            VStack(spacing: 24){
+        VStack(spacing: 40){
+            VStack(spacing: 20){
                 Text("Enter Passcode")
                     .font(.largeTitle)
                     .fontWeight(.heavy)
@@ -35,17 +47,30 @@ struct PasscodeView: View {
                 Text("Please enter your 4-digit pin to securely access your account.")
                     .font(.subheadline)
                     .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .frame(height: 60)
             } .padding(.top)
             
             // indicator view
-            PasscodeIndicatorView(passcode: $passcode)
+            PasscodeIndicatorView(passcode: $passcode,isAnimation: $isAnimation)
+            
             Spacer()
+            
+            if isWrong{
+                Text("Wrong password, please re-enter")
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+            }
+            
             //numberpad view
             NumberPadView(passcode: $passcode)
+            
         }.onChange(of: passcode) { _ , _ in
             verifyPasscode()
         }
     }
+    
     private func verifyPasscode(){
         guard passcode.count == 4 else{ return }
         
@@ -53,6 +78,10 @@ struct PasscodeView: View {
             try? await Task.sleep(nanoseconds: 125_000_000)
             isAuthenticated = passcode == "1111"
             passcode = ""
+            withAnimation(.spring) {
+                self.isAnimation = true
+                isWrong = true
+            }
         }
     }
 }
